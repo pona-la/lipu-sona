ce0446975699a242ad394bc3853a7b0588c4c492
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2024-08-20 21:07:39 +0500

    ci: move to bin
    Clean up root folder

--- bin/simplify.sh
+++ bin/simplify.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# Appears to be in disuse.
+
 perl -0pe 's/<div class="warning">(.*?)<\/div>/<table bgcolor="#f5ee9b" border="1px solid #9b6e2d"><tbody><tr><td>\1<\/td><\/tr><\/tbody><\/table>/gs' | \
 perl -0pe 's/<div class="info">(.*?)<\/div>/<table bgcolor="#8be1e0" border="1px solid #221f31"><tbody><tr><td>\1<\/td><\/tr><\/tbody><\/table>/gs' | \
 perl -0pe 's/<div class="error">(.*?)<\/div>/<table bgcolor="#ea9182" border="1px solid #a14d3f"><tbody><tr><td>\1<\/td><\/tr><\/tbody><\/table>/gs'
